d282d4edf7ff57680eaec796b23b634aad8f05d9
Paresh Salunke <pareshsalunke@gmail.com>
2026-03-05 14:54:51 +0100

    remove duplicate import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type {Metadata} from 'next';
-import { Analytics } from "@vercel/analytics/next"
 import './globals.css';
 import ThemeProvider from '@/components/ThemeProvider';
 import {Analytics} from '@vercel/analytics/next';
